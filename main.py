d5f185b008d04ed4cf2245af3b0a318a77f01a68
dehatanes <debora.buss@hotmail.com>
2018-10-06 17:43:41 -0700

    Add command line inputs to deploy

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import requests
+import sys, getopt
 from BotHandler import BotHandler
 
 if __name__ == '__main__':
-        bot_token = input("Token: ")       # <-- Token recebido pelo botFather no telegram
-        my_bot = BotHandler(bot_token)     # <-- Configurando o meu bot
-        my_bot.run()
+    # SETUP
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"",longopts=["token"])
+        bot_token = args[0]            # <-- Token recebido por parâmetro ao iniciar o app
+                                       #     conseguido através do botFather no telegram
+    except getopt.GetoptError:
+        print()
+        print('Wrong usage!')
+        print("Use the parameter --token <BOT_TOKEN> to run.")
+        print()
+        sys.exit()
+    # START THE BOT
+    my_bot = BotHandler(bot_token)     # <-- Configurando o meu bot
+    my_bot.run()                       # <-- IT'S ALIVE!
