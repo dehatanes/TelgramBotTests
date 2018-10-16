64ff06f1fb857fc406d347da15c86912a3967476
Débora Atanes <debora.buss@hotmail.com>
2018-10-16 01:55:24 -0300

    Trannsformando aplicação em restful
    Para possibilitar adicionar webhooks no bot do telegram

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import sys, getopt
 from BotHandler import BotHandler
 
 if __name__ == '__main__':
+    #-----------
     # SETUP
+    #-----------    
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"",longopts=["token"])
+        opts, args = getopt.getopt(sys.argv[1:],"",longopts=["token"]) # parametrizar o webhook depois
         bot_token = args[0]            # <-- Token recebido por parâmetro ao iniciar o app
                                        #     conseguido através do botFather no telegram
     except getopt.GetoptError:
@@ -13,6 +15,10 @@ if __name__ == '__main__':
         print("Use the parameter --token <BOT_TOKEN> to run.")
         print()
         sys.exit()
+
+    #----------------
     # START THE BOT
-    my_bot = BotHandler(bot_token)     # <-- Configurando o meu bot
-    my_bot.run()                       # <-- IT'S ALIVE!
+    #----------------
+    app = BotHandler(bot_token)                  # <-- Configurando o meu bot
+    port = int(os.environ.get("PORT", 5000))     # <-- Como o Heroku muda de porta o tempo todo temos qu nos adaptar
+    app.run(host='0.0.0.0', port=port)           # <-- IT'S ALIVE!
