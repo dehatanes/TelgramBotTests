984f5eeda3a8eb542aefcf45cc4f5bfc4884f633
Débora Atanes <debora.buss@hotmail.com>
2018-10-20 22:29:16 -0300

    Solving identation errors

--- scheduled_actions.py
+++ scheduled_actions.py
@@ -2,11 +2,11 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
 
 def timed_job():
-  bot_token = os.environ.get("TOKEN")          # <-- Token nas variavais de ambiente do Heroku
-    if(bot_token):
+	bot_token = os.environ.get("TOKEN")          # <-- Token nas variavais de ambiente do Heroku
+	if(bot_token):
 		bot_send_message = "https://api.telegram.org/bot{0}/sendMessage".format(bot_token)
 		params = {'chat_id': os.environ.get("DEFAULT_USER_CHAT_ID"),
-                  'text': "I am still alive :)"}
+				  'text': "I am still alive :)"}
 		requests.get(bot_send_message, params)
 
 sched = BlockingScheduler()
