4c019739fc01a442cd196fef66b7580d21aa1720
Débora Atanes <debora.buss@hotmail.com>
2018-10-27 04:26:07 -0300

    Add logs

--- DatabaseUtils.py
+++ DatabaseUtils.py
@@ -8,13 +8,15 @@ class MongoDB:
     db = conn[urlparse(Constants.MONGO_URL).path[1:]]
 
     def insertNewUser(user_infos):
-    	user = {}
-    	user["first_name"] = user_infos.get("first_name")
-    	user["last_name"] = user_infos.get("last_name")
-    	user["username"] = user_infos.get("username")
-    	user["id"] = user_infos.get("id")
-    	user["interactions_count"] = 1
-    	db.test_collection.insert(user)
+    	print("NEW USER INSERTED")
+		user = {}
+		user["first_name"] = user_infos.get("first_name")
+		user["last_name"] = user_infos.get("last_name")
+		user["username"] = user_infos.get("username")
+		user["id"] = user_infos.get("id")
+		user["interactions_count"] = 1
+		db.test_collection.insert(user)
 
-    def getAllData():
-		print db.test_collection.find()
+	def getAllData():
+		print("GETTING ALL THE DATA")
+		return db.test_collection.find()
